310104dc6aad25ba32ad92f7329e89c13df8c7b3
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 17:50:24 -0800

    Added update_restaurant_and_ratings function

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,4 +1,5 @@
 def restaurant_ratings(file_name):
+    """Prints out restaurant name and rating.""" 
 
     data = open(file_name)
     restaurant_and_rating = {}
@@ -11,8 +12,24 @@ def restaurant_ratings(file_name):
     dictionary_to_tuple = restaurant_and_rating.iteritems()
 
     for rn_key, rate_value in dictionary_to_tuple:
-        print "%s is rated at %s." % (rn_key, rate_value)
+        # print "%s is rated at %s." % (rn_key, rate_value)
+
+        return restaurant_and_rating
+
+def update_restaurant_and_ratings():
+    """Asks user for new restaurant and rating and adds it to dictionary from restaurant_ratings()"""
+
+    user_restaurant_name = raw_input("Enter restaurant name: ")
+    user_restaurant_rating = raw_input("Enter restaurant rating: ")
+
+    restaurant_and_rating = restaurant_ratings("scores.txt")
+
+    restaurant_and_rating[user_restaurant_name] = int(user_restaurant_rating)
+
+    sorted_restaurants_alphabetically = sorted(restaurant_and_rating)
+
+    for restaurant in sorted_restaurants_alphabetically:
+        print "%s %s" % (restaurant, restaurant_and_rating[restaurant])
 
 
-
-restaurant_ratings("scores.txt")
+update_restaurant_and_ratings()
